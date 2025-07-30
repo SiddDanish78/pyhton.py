9fcfaa8ef939518cee33e38a81b904b856fd13f1
SiddDanish78 <danishseddque5@gmail.com>
2025-07-30 14:07:57 +0530

    Add files via upload

---
+++ C2-02 DATATYPES.py
@@ -0,0 +1,9 @@
+a = 1 # a is an integer
+
+b = 7.33 # b is a floating point number
+
+c = "danish"  # c is a string 
+
+d = False  # d is a boolean variable 
+
+e = None #  e is a none type variable
---
+++ C2-04Operators.py
@@ -0,0 +1,59 @@
+# Arithmetic operators 
+a = 5
+r = 3.2
+c = a+r
+print (c)
+
+# some more example of arthmetic operators 
+e = 58
+g = 926352
+c = g/e
+print (c)
+
+# now assignment operators 
+
+a = 4-2 
+print (a)  # normal calculation 
+a = 4-3 # assign 4-2 in a 
+b = 7 
+# b += 6 # increment the value of b by 6 and then assing it to b 
+# b -= 7 # decrement the value of b by 7 and then assing it to b
+b *= 5 # multiply the vlaue of b by 5 then assing it to b   
+print (b)
+
+# now comparison operators 
+# o = 5 == 5  
+# o = 6>7 
+# o = 45 < 65 
+# o = 2 >= 5
+o = 7 != 5
+print (o)
+
+# logical operators 
+ 
+e = True or False 
+print (e)
+
+# truth table of "or"
+
+print ("True or False " , True or False)
+print ("True or True " , True or True)
+print ("False or true " , False or True)
+print ("False or False " , False or False)
+
+# truth table of "and"
+
+print ("True and  False " , True and  False)
+print ("True and  True " , True and  True)
+print ("False and  true " , False and True)
+print ("False and  False " , False and  False)
+
+# truth table of not 
+
+print(not(False))
+print (not(True))
+
+
+
+
+
