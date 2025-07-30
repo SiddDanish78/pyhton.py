9b15353942be7afcbd16e5f9325d8fc0000c672a
SiddDanish786 <danishseddque3@gmail.com>
2025-07-30 11:35:09 +0530

    initial commit

---
+++ C-2 02 module.py
@@ -0,0 +1,12 @@
+import pyjokes
+print("printing jokes")
+# this prints a random jokes(using of ctrl+/ to ignore this comment)
+joke = pyjokes.get_joke()
+print(joke)
+"""
+you can comment
+out multiple 
+line by using also 3 
+lagatar comms
+and you can also comment out multiple line by select all line and ctrl+\
+"""
